880f4a96c8308a122f4697162a7b62d8e6369ea5
Danting Huang <serena.huangdanting@gmail.com>
2022-08-20 16:22:56 -0400

    add tests for status code check for routes

--- server/QnAServiceSpec/server.test.js
+++ server/QnAServiceSpec/server.test.js
@@ -1,25 +1,53 @@
 /* eslint-disable no-undef */
 const request = require('supertest');
 const app = require('../server');
+const pool = require('../../db/postgres/index');
 
-describe('Test root', () => {
-  it('GET /', async () => {
+afterAll(async () => {
+  await pool.end();
+});
+
+describe('Test routes', () => {
+  it('GET / should send status code 200 and correct message', async () => {
     const res = await request(app)
       .get('/');
     // console.log(res);
     expect(res.status).toEqual(200);
     expect(res.text).toEqual('questions and answers!');
   });
-});
 
-describe('Test sample questions response', () => {
-  it('GET /qa/questions', async () => {
+  it('GET /qa/questions should send status code 200', async () => {
     const res = await request(app)
-      .get('/qa/questions')
-      .set('Accept', 'application/json');
-    // console.log(res.body.data.results);
+      .get('/qa/questions');
+    // console.log(res);
     expect(res.status).toEqual(200);
-    expect(res.body.data.results.length).toEqual(16);
-    expect(res.body.data.product_id).toEqual('71697');
+  });
+
+  it('GET /qa/questions/:question_id/answers should send status code 200', async () => {
+    const res = await request(app)
+      .get('/qa/questions/:question_id/answers');
+    // console.log(res);
+    expect(res.status).toEqual(200);
+  });
+
+  it('POST /qa/questions/:question_id/answers should send status code 201 and expected message', async () => {
+    const res = await request(app)
+      .post('/qa/questions/:question_id/answers');
+    // console.log(res);
+    expect(res.status).toEqual(201);
+    expect(res.text).toEqual('Answer added!');
   });
 });
+
+/** ******* old sample data test ********************** */
+// describe('Test sample questions response', () => {
+//   it('GET /qa/questions should send status code 200 and correct sample data ', async () => {
+//     const res = await request(app)
+//       .get('/qa/questions')
+//       .set('Accept', 'application/json');
+//     // console.log(res.body.data.results);
+//     expect(res.status).toEqual(200);
+//     expect(res.body.data.results.length).toEqual(16);
+//     expect(res.body.data.product_id).toEqual('71697');
+//   });
+// });
